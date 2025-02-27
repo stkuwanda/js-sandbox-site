868c2681c1eb82eb86eaad766fec59220e157762
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-27 08:02:24 +0200

    added ability to delete cats, reset input after submission

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ let cats = [];
 // Function which returns a li element with the content inside
 function listItemFactory(value) {
 	const li = document.createElement('li');
-	li.append(value);
+	const button = document.createElement('button');
+	button.append('Delete');
+
+	button.addEventListener('click', function() {
+		deleteCat(value);
+	});
+
+	li.append(value, button);
 	return li;
 }
 
@@ -47,12 +54,23 @@ function addCat(catName) {
 	updateCatDisplay(); //call function to update the users display.
 }
 
+// Function to delete a cat from the array
+function deleteCat(catName) {
+	const index = cats.indexOf(catName);
+
+	if (index > -1) {
+		cats.splice(index, 1);
+		saveCatsToLocalStorage();
+		updateCatDisplay();
+	}
+}
+
 //Example function to update the user display
 function updateCatDisplay() {
 	const list = document.querySelector('ul#cats');
 	list.innerHTML = ''; // clear list
 
-	for(const cat of cats) {
+	for (const cat of cats) {
 		list.append(listItemFactory(cat));
 	}
 }
@@ -78,4 +96,5 @@ form.addEventListener('submit', function (event) {
 	event.preventDefault();
 	console.log(input.value);
 	addCat(input.value);
+	input.value = '';
 });
